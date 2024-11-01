08ddc15d96a0ab1eaeaf025282e248486947d186
ayoubAmazu <ayoubamazu@gmail.com>
2024-11-01 13:33:32 +0100

    liveCoding done

---
+++ maitrise-eloquent/liveCoding/app/Models/articl.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class articl extends Model
+{
+    /** @use HasFactory<\Database\Factories\ArticlFactory> */
+    use HasFactory;
+}
